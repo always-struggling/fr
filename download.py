954f6badaa669e9d26284b987d1411dc8c21cc94
always_struggling <r_manteghi@hotmail.co.uk>
2017-01-12 23:44:03 +0000

    download: Added function to download, append ID3 tages, and move songs.

--- download.py
+++ download.py
@@ -4,6 +4,22 @@ import eyed3
 
 class Download(object):
 
+    def __init__(self):
+        self.music_dir = 'C:\\Users\\User\\Documents\\python\\projects\\fr\\music_test\\'
+
+    def get_song(self, url, title, song, artist, playlist, album_art):
+        '''
+        Top level function for downloading a song, appending ID3 tags,
+        and moving it into the correct directory
+        :return:
+        '''
+        song_downloaded = self.download_song(url)
+        if song_downloaded:
+            tmp_filename = os.getcwd() + '\\' + title + '-' + url + '.mp3'
+            self.append_tags(tmp_filename, song, artist, playlist, album_art)
+            self.move_song(tmp_filename, self.music_dir + song + '.mp3')
+
+
     def download_song(self, url):
         '''
         This function downloads the audio of a youtube video
@@ -25,3 +41,32 @@ class Download(object):
             except:
                 song_downloaded = False
         return song_downloaded
+
+    def append_tags(self, mp3, song, artist, playlist, album_art):
+        '''
+        Appends ID3 tags to the mp3 file.
+        :param filename: The mp3 we plan to append tags to
+        :param song: The name of the song.
+        :param artist: The name of the artist
+        :param album_art: A file path to the album art.
+        '''
+        mp3_file = eyed3.load(mp3)
+        mp3_file.tag.title = unicode(song)
+        mp3_file.tag.artist = unicode(artist)
+        mp3_file.tag.album = unicode(playlist)
+        image_data = open(album_art, 'rb').read()
+        mp3_file.tag.images.set(3, image_data, 'image/jpeg', u'Album art')
+        '''
+        Windows is annoying so we have to save the ID3 tags in some other version.
+        I still can't get the pictures to work on Media Player - don't really care though
+        '''
+        #mp3_file.tag.save(mp3, version=(1, None, None))
+        mp3_file.tag.save(mp3, version=(2, 4, 0))
+
+    def move_song(self, orig_filepath, new_filepath):
+        '''
+        As a result of youtube_dl being a bit shit, we have to move the song
+        :return:
+        '''
+        os.rename(orig_filepath, new_filepath)
+
--- test.py
+++ test.py
@@ -1,25 +1,28 @@
 import unittest
 import os
+import shutil
 from PIL import Image
 from music_db import MusicDB
 from scrape import Scrape
 from fix_title import Fix
 from album_art import Art
+from download import Download
+import os
+import eyed3
+
 
 
 class Setup(unittest.TestCase):
-
     def test_db(self):
         db = MusicDB()
         db.create_database_structure()
         db.insert_artist_data('Test Artist', '/test_path/test_artist.jpg')
         db.insert_artist_data('MC RudeBoi', None)
         all_artists = db.get_all_artists()
-        self.assertEqual(all_artists, ['Free Riddims','MC RudeBoi', 'Test Artist'])
+        self.assertEqual(all_artists, ['Free Riddims', 'MC RudeBoi', 'Test Artist'])
 
 
 class TestFixer(unittest.TestCase):
-
     def test_fix_by_delimiter(self):
         testcase_fix = Fix()
         split_song = testcase_fix.split_song_via_delim('Test Artist - Test Song')
@@ -80,35 +83,33 @@ class TestFixer(unittest.TestCase):
 
 
 class TestScraper(unittest.TestCase):
-
     def test_scrape_playlists(self):
         testcase_scrape = Scrape()
         playlists = testcase_scrape.get_all_playlists('UCztBf-iBKrDBYw4BzNK5B1Q')
         self.assertEqual(playlists, [['PLuXrQvHb49n4pHUoMYBLG5cVtrXhngcKR', 'playlist2']
-                                     , ['PLuXrQvHb49n6jqOq9ftPHC8LVqDF5kG1X', 'playlist1']
-                                     , ['LLztBf-iBKrDBYw4BzNK5B1Q', 'Likedvideos']
+            , ['PLuXrQvHb49n6jqOq9ftPHC8LVqDF5kG1X', 'playlist1']
+            , ['LLztBf-iBKrDBYw4BzNK5B1Q', 'Likedvideos']
                                      ])
 
     def test_scrape_songs(self):
         testcase_scrape = Scrape()
         songs = testcase_scrape.get_all_songs('PLuXrQvHb49n4pHUoMYBLG5cVtrXhngcKR')
         self.assertEqual(songs, [['JRWox-i6aAk', 'Lana Del Rey - Blue Jeans']
-                                 , ['oKxuiw3iMBE', 'Lana Del Rey - West Coast']
+            , ['oKxuiw3iMBE', 'Lana Del Rey - West Coast']
                                  ])
 
     def test_scrape_all_songs(self):
         testcase_scrape = Scrape()
         songs = testcase_scrape.set_youtube_songs('UCztBf-iBKrDBYw4BzNK5B1Q')
         self.assertEqual(songs, [['JRWox-i6aAk', 'Lana Del Rey - Blue Jeans', 'playlist2']
-                                 , ['oKxuiw3iMBE', 'Lana Del Rey - West Coast', 'playlist2']
-                                 , ['_z-1fTlSDF0', 'Happy Birthday song', 'playlist1']
-                                 , ['Bag1gUxuU0g', 'Lana Del Rey - Born To Die', 'playlist1']
-                                 , ['vrlTeoFcf-Q', '[Private video]', 'Likedvideos']
+            , ['oKxuiw3iMBE', 'Lana Del Rey - West Coast', 'playlist2']
+            , ['_z-1fTlSDF0', 'Happy Birthday song', 'playlist1']
+            , ['Bag1gUxuU0g', 'Lana Del Rey - Born To Die', 'playlist1']
+            , ['vrlTeoFcf-Q', '[Private video]', 'Likedvideos']
                                  ])
 
 
 class TestArt(unittest.TestCase):
-
     def test_search_existing_art(self):
         db = MusicDB()
         all_art = dict(db.get_all_art())
@@ -118,7 +119,7 @@ class TestArt(unittest.TestCase):
     def test_get_art_url_found(self):
         art = Art()
         image_url, image_path_found = art.find_art2('bowie')
-        self.assertEqual(image_url, 'https://images-na.ssl-images-amazon.com/images/I/31inGXqqWsL.jpg')
+        self.assertEqual(image_url, 'https://images-na.ssl-images-amazon.com/images/I/618%2Bo-mU-sL.jpg')
         self.assertEqual(image_path_found, True)
 
     def test_get_art_url_not_found(self):
@@ -142,16 +143,50 @@ class TestArt(unittest.TestCase):
         art = Art()
         image_path = art.get_album_art('bowie')
         self.assertEqual(image_path, 'album_art\\bowie.jpg')
+        os.remove('album_art\\bowie.jpg')
 
     def test_get_image_found(self):
         art = Art()
         image_path = art.get_album_art('adele')
         self.assertEqual(image_path, 'album_art\\adele.jpg')
+        os.remove('album_art\\adele.jpg')
 
     def test_get_image_not_found(self):
         art = Art()
         image_path = art.get_album_art('dslfknsdlg')
         self.assertEqual(image_path, 'album_art\\free_riddims_default.jpg')
+        os.remove('album_art\\free_riddims_default.jpg')
+
+
+class TestDownload(unittest.TestCase):
+    def test_1_download_song(self):
+        dwn = Download()
+        dwn.download_song('NCFg7G63KgI')
+        self.assertEqual(os.path.isfile('The funniest 10 second video ever-NCFg7G63KgI.mp3'), True)
+        os.remove('The funniest 10 second video ever-NCFg7G63KgI.mp3')
+
+    def test_2_append_tags(self):
+        dwn = Download()
+        shutil.copy('music_test\\test.mp3', 'test_append.mp3')
+        filename = os.getcwd() + '\\test_append.mp3'
+        art = 'C:\\Users\\User\\Documents\\python\\projects\\fr\\album_art\\free_riddims_default.jpg'
+        dwn.append_tags(filename, 'Test Song', 'Test Artist', 'Test Playlist', art)
+        os.remove(filename)
+
+    def test_3_move_song(self):
+        dwn = Download()
+        shutil.copy('music_test\\test.mp3', 'test_move.mp3')
+        tmp_filename = os.getcwd() + '\\test_move.mp3'
+        new_filename = os.getcwd() + '\\music_test\\test move.mp3'
+        dwn.move_song(tmp_filename, new_filename)
+        self.assertEqual(os.path.isfile('music_test\\test move.mp3'), True)
+        os.remove('music_test\\test move.mp3')
+
+    def test_4_get_song(self):
+        dwn = Download()
+        dwn.get_song('NCFg7G63KgI', 'The funniest 10 second video ever', 'test song', 'test artist', 'test playlist', 'album_art\\free_riddims_default.jpg')
+        self.assertEqual(os.path.isfile('music_test\\test song.mp3'), True)
+        os.remove('music_test\\test song.mp3')
 
 if __name__ == '__main__':
     unittest.main()
